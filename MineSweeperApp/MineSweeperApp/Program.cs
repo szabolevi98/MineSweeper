ba67c720efed181d972004eb4ea3e719fb481557
Levente <Levente@DESKTOP-9AOSBH5>
2020-04-03 21:58:21 +0200

    2020.04.03.

--- MineSweeperApp/MineSweeperApp/Program.cs
+++ MineSweeperApp/MineSweeperApp/Program.cs
@@ -11,7 +11,7 @@ namespace MineSweeperApp
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
+        //[STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
